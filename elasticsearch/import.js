6f9d88138b6888146c74fc262c34140d9fd29cd7
icyks <kevin.keovilay@gmail.com>
2018-02-20 18:03:52 +0100

    first ver

--- elasticsearch/import.js
+++ elasticsearch/import.js
@@ -2,16 +2,79 @@ var elasticsearch = require('elasticsearch');
 var csv = require('csv-parser');
 var fs = require('fs');
 
-var esClient = new elasticsearch.Client({
+var client = new elasticsearch.Client({
   host: 'localhost:9200',
   log: 'error'
 });
 
+
+
+// Création de l'indice
+client.indices.create({ index: '911calls' }, (err, resp) => {
+  if (err) 
+  	{ 
+  		console.trace(err.message);
+  		return;
+  	}
+  client.indices.putMapping({
+  		index:'911calls',
+  		type: "call",
+  		body: {
+  			call: {
+  				properties: {
+  					location : {
+  						type: "geo_point"
+  					}
+  				}
+  			}
+  		}
+  	}); 
+});
+
+
+let calls = [];
 fs.createReadStream('../911.csv')
     .pipe(csv())
     .on('data', data => {
-      // TODO extract one line from CSV
-    })
+    	let title = data.title.split(":");
+        let timeStamp = data.timeStamp.split(" ");
+        let date = timeStamp[0].split("-");
+		calls.push({
+                location:{
+                	lon:+data.lng,
+                    lat: +data.lat
+                },
+                desc:data.desc,
+                zip:data.zip,
+               	cat:title[0],
+                title:title[1],
+                date:date[0]+"/"+date[1],
+                time:data.timeStamp,
+                twp:data.twp,
+                addr:data.addr,
+                e:data.e
+        });
+	})
     .on('end', () => {
-      // TODO insert data to ES
+	  	client.bulk(createBulkInsertQuery(calls), (err, resp) => {
+		    if (err) console.trace(err.message);
+		    else console.log(`Inserted ${resp.items.length} calls`);
+		    client.close();
+	    });
     });
+
+
+
+// Fonction utilitaire permettant de formatter les données pour l'insertion "bulk" dans elastic
+function createBulkInsertQuery(calls) {
+  const body = calls.reduce((acc, call) => {
+    const { location, desc,zip,cat,title,date,time,twp,addr,e } = call;
+    acc.push({ index: { _index: '911calls', _type: 'call'} })
+    acc.push({ location, desc,zip,cat,title,date,time,twp,addr,e })
+    return acc
+  }, []);
+
+  return { body };
+}
+
+
--- mongodb/import.js
+++ mongodb/import.js
@@ -12,7 +12,28 @@ var insertCalls = function(db, callback) {
     fs.createReadStream('../911.csv')
         .pipe(csv())
         .on('data', data => {
-            var call = {}; // TODO créer l'objet call à partir de la ligne
+            let title = data.title.split(":");
+            let timeStamp = data.timeStamp.split(" ");
+            let date = timeStamp[0].split("-");
+            var call = {
+                "lat":data.lat,
+                "lng":data.lng,
+                "location": {
+                    type: "Point",
+                    coordinates: [+data.lng,+data.lat]
+                },
+                "desc":data.desc,
+                "zip":data.zip,
+                "cat":title[0],
+                "title":title[1],
+                "year":date[0],
+                "month":date[1],
+                "day":date[2],
+                "time":timeStamp[1],
+                "twp":data.twp,
+                "addr":data.addr,
+                "e":data.e
+            }; 
             calls.push(call);
         })
         .on('end', () => {
@@ -22,9 +43,77 @@ var insertCalls = function(db, callback) {
         });
 }
 
+var createTextIndex = function(db, callback) {
+  // Get the documents collection
+  var collection = db.collection('calls');
+  // Create the index
+  collection.createIndex(
+    { title : "text" }, function(err, result) {
+    console.log(result + " textindex created");
+    callback(result);
+  });
+};
+
+var create2DSphereIndex = function(db, callback) {
+  // Get the documents collection
+  var collection = db.collection('calls');
+  // Create the index
+  collection.createIndex(
+    { location : "2dsphere" }, function(err, result) {
+        if (err) {
+            console.log(err);
+            throw err;
+        }
+    console.log(result+ "2dsphereindex created");
+    callback(result);
+  });
+};
+
+var createIndexMonth = function(db, callback) {
+  // Get the documents collection
+  var collection = db.collection('calls');
+  // Create the index
+  collection.createIndex(
+    { month : 1 }, function(err, result) {
+    console.log(result + "index created");
+    callback(result);
+  });
+};
+
+var createIndexYear = function(db, callback) {
+  // Get the documents collection
+  var collection = db.collection('calls');
+  // Create the index
+  collection.createIndex(
+    { year : 1 }, function(err, result) {
+    console.log(result + "index created");
+    callback(result);
+  });
+};
+var createIndexCat = function(db, callback) {
+  // Get the documents collection
+  var collection = db.collection('calls');
+  // Create the index
+  collection.createIndex(
+    { cat : 1 }, function(err, result) {
+    console.log(result + "index created");
+    callback(result);
+  });
+};
+
 MongoClient.connect(mongoUrl, (err, db) => {
     insertCalls(db, result => {
         console.log(`${result.insertedCount} calls inserted`);
-        db.close();
+        createTextIndex(db, result1=> {
+            createIndexCat(db, result2=> {
+                createIndexMonth(db, result3=> {
+                    createIndexYear(db, result4=> {
+                        create2DSphereIndex(db, result5=> {
+                            db.close();
+                        })
+                    })
+                })
+            })
+        })
     });
 });
